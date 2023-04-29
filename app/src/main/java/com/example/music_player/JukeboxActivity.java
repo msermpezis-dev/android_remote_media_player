09caf066cb6d7c0b3dc83a669510e0bac3300b34
msermpezis-dev <emmanouil.sermpezis@gmail.com>
2023-04-29 15:45:27 +0300

    added onAction events

--- app/src/main/java/com/example/music_player/JukeboxActivity.java
+++ app/src/main/java/com/example/music_player/JukeboxActivity.java
@@ -4,19 +4,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.media.AudioAttributes;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ui.AppBarConfiguration;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,24 +19,21 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.music_player.databinding.ActivityMainBinding;
 
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Arrays;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class JukeboxActivity extends AppCompatActivity {
 
-    private Button btn_play;
-    private Button btn_pause;
-    private Button btn_request;
+    private ImageButton btn_play;
+    private ImageButton btn_pause;
+    private ImageButton btn_request;
     private TextView tv_status_view;
     private final MediaPlayer mediaPlayer = new MediaPlayer();;
     private int mediaFileLengthInMilliseconds;
@@ -121,12 +113,12 @@ public class JukeboxActivity extends AppCompatActivity {
         playSong();
     }
 
-    private void disableButton(Button button){
+    private void disableButton(ImageButton button){
         button.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP);
         button.setEnabled(false);
         button.setClickable(false);
     }
-    private void enableButton(Button button){
+    private void enableButton(ImageButton button){
         button.getBackground().setColorFilter(null);
         button.setEnabled(true);
         button.setClickable(true);
@@ -224,5 +216,35 @@ public class JukeboxActivity extends AppCompatActivity {
     public void goToMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        pauseSong();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        pauseSong();
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        pauseSong();
+
     }
 }
